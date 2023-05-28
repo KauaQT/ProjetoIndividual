60ebf3e8375a6af4252b7a9bf1bb3bd82055ccff
iniciandokaua <kaua.teles@sptech.school>
2023-05-28 01:27:30 -0300

    Tela de sugestões completa

--- AdreaBocelli/public/js/desafio.js
+++ AdreaBocelli/public/js/desafio.js
@@ -1,3 +1,25 @@
-var validacao1 = document.getElementById("#validacao")
+const elementyMinutes = document.querySelector("#minutes");
+const elementySeconds = document.querySelector("#seconds");
+const startbtn = document.querySelector("#startbtn");
 
-console.log(validacao1)
+
+let interval;
+let minutes = 0;
+let seconds = 0;
+
+startbtn.addEventListener('click', startTimer)
+
+function startTimer() {
+
+    interval = setInterval(() => {
+
+        seconds += 100;
+
+        if (seconds === 60) {
+            minutes++;
+            seconds = 0;
+        }
+
+    }, 100)
+
+}
