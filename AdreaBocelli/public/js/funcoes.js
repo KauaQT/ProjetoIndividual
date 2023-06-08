b1c592e8f55a84934f3e652bed8b029e0349c716
iniciandokaua <kaua.teles@sptech.school>
2023-06-08 17:02:16 -0300

    Validação completa do desafio, detalhes ajustados em algumas telas

--- AdreaBocelli/public/js/funcoes.js
+++ AdreaBocelli/public/js/funcoes.js
@@ -53,3 +53,63 @@ function fecharModal() {
     divModal.style.display = "none";
 }
 
+const carousel = document.querySelector(".carousel");
+firstImg = carousel.querySelectorAll("img")[0]; 
+arrowIcons = document.querySelectorAll(".wrapper i");
+
+let isDragStart = false, prevPageX, prevScrollLeft
+
+const showHideIcons = () => {
+    let scrollWidth = carousel.scrollWidth - carousel.clientWidth;
+    if (carousel.scrollLeft == 0) {
+        arrowIcons[0].style.display = "none";
+    } else {
+        arrowIcons[0].style.display = "block"
+    }
+    
+    if (carousel.scrollLeft == 1) {
+        arrowIcons[1].style.display = "none";
+    } else {
+        arrowIcons[1].style.display = "block"
+    }
+}
+
+arrowIcons.forEach(icon => {
+    icon.addEventListener("click", () => {
+// carousel;scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+    let firstImgWidth = firstImg.clientWidth + 14
+        if (icon.id == "left") {
+            carousel.scrollLeft -= firstImgWidth;
+        } else {
+            carousel.scrollLeft += firstImgWidth;
+        }
+        setTimeout(() => showHideIcons(), 60)
+        console.log(icon)
+    })
+});
+    
+const dragStart = (e) => {
+    isDragStart = true;
+    prevPageX = e.pageX
+    prevScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e) => {
+    // console.log(e.pageX);
+    if (!isDragStart) return;
+    e.preventDefault();
+    carousel.classList.add("dragging");
+    let positionDiff = e.pageX - prevPageX;
+    carousel.scrollLeft = prevScrollLeft - positionDiff;
+}
+
+const dragStop = () => {
+    isDragStart = false;
+    carousel.classList.remove("dragging");
+    showHideIcons();
+}
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("mouseup", dragStop);
+carousel.addEventListener("mouseleave", dragStop);
